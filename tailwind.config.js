ce939597ad24f8da61a5c381dcdc814762d48613
Aditya Lohar <77808500+AdityaLohar@users.noreply.github.com>
2024-08-08 22:28:01 +0530

    fixed issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ export default {
         'inner-left-right': 'inset -8px 0 8px -8px rgba(0, 0, 0, 0.3), inset 8px 0 8px -8px rgba(0, 0, 0, 0.3)',
       },
       fontFamily: {
-        sans: ['Roboto', 'sans-serif'],
+        sans: ['Satoshi' ,'Roboto', 'sans-serif'],
       },
     },
   },
